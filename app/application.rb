6e4591eedb0f8c71df09d1db80b90eeecca32dc7
John Zonneveld <jjczon@gmail.com>
2020-05-17 14:17:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Application
   
   attr_accessor :name, :price
@@ -10,8 +11,9 @@ class Application
     if req.path.match(/items/)
       item = Item.new(name, price)
       @@item << item
-      for @@item.each do |i|
-        resp.write "#{i.name} #{i.price}"
+      for @@item.each do |i|{
+        binding.pry
+        resp.write "#{i.name} #{i.price}"}
       end
     else 
       return [ 404, {'Content-Type' => 'text/html'}, "Route not found" ]
