eeddbf8a9ef6a954431ca983370ceb1aac719617
John Zonneveld <jjczon@gmail.com>
2020-05-17 14:40:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,14 +11,6 @@ class Application
       item_name = req.path.split("/items/").last
       # puts "item_name = #{item_name}"
       item = @@items.detect { |i| i.name == item_name }
-      iterm_term = req.params["q"]
-      puts iterm_term
-      puts name
-      puts price
-      @@item << Item.new(name, price)
-      i = @@item.last
-      puts @@item
-      puts i.price
       resp.write "#{i.price}"
     else 
       return [ 404, {'Content-Type' => 'text/html'}, "Route not found" ]
