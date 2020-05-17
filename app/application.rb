d40021189e6d2c19dd604fc02fe7ddac7e80e30b
John Zonneveld <jjczon@gmail.com>
2020-05-17 14:24:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       @@item << Item.new(name, price)
       i = @@item.last
       puts @@item
-      puts i.name
+      puts i.price
       resp.write "#{i.price}"
     else 
       return [ 404, {'Content-Type' => 'text/html'}, "Route not found" ]
