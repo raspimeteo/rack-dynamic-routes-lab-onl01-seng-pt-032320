ddc17a4349a4b2e60a5f47efe8e469d733d16783
John Zonneveld <jjczon@gmail.com>
2020-05-17 14:13:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
 class Application
   
   attr_accessor :name, :price
+  
+  @@item = []
 	 
   def call(env)
     resp = Rack::Response.new
@@ -8,7 +10,8 @@ class Application
     if req.path.match(/items/)
       item_info = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
       item = Item.new(name, price)
-      resp.write "#{item.name} #{item.price}"
+      @@item << item
+      resp.write "#{@@item.name} #{@@item.price}"
     else 
       return [ 404, {'Content-Type' => 'text/html'}, "Route not found" ]
     end
