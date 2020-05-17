f44bbd3d8f49594e0affaae1096a95e608d3b204
John Zonneveld <jjczon@gmail.com>
2020-05-17 14:03:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,20 +1,14 @@
 class Application
 	 
-  @@songs = [Song.new("Sorry", "Justin Bieber"),
-            Song.new("Hello","Adele")]
-5.	 
-6.	  def call(env)
-7.	    resp = Rack::Response.new
-8.	    req = Rack::Request.new(env)
-9.	 
-10.	    if req.path.match(/songs/)
-11.	 
-12.	      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-13.	      song = @@songs.find{|s| s.title == song_title}
-14.	 
-15.	      resp.write song.artist
-16.	    end
-17.	 
-18.	    resp.finish
-19.	  end
-20.	end
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match(/items/)
+      item_info = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+      resp.write song.artist
+    end
+	 
+	    resp.finish
+  end
+end
