7d7739cc884545e57fbf3abc8627c2fd7adb5791
John Zonneveld <jjczon@gmail.com>
2020-05-17 15:06:14 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+class Application
+  
+  # attr_accessor :name, :price
+  
+  @@items = []
+	 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.detect { |i| i.name == item_name }
+        resp.write item.price
+      else
+        return [ 400, {'Content-Type' => 'text/html'}, "Item not found" ]
+      end
+    else 
+      return [ 404, {'Content-Type' => 'text/html'}, "Route not found" ]
+    end
+	    resp.finish
+  end
+end
